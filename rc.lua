51447c6cc6e8b8beb17262fa4994d37e16e2ceb7
schrodit <mail@timschrodi.tech>
2021-11-19 16:56:46 +0100

    add icons to rofi drun

--- rc.lua
+++ rc.lua
@@ -321,7 +321,7 @@ globalkeys = mytable.join(
               {description = "show main menu", group = "awesome"}),
     
     -- Rofi App Launcher
-    awful.key({ "Control" }, "space",  function () awful.spawn("rofi -show drun") end,
+    awful.key({ "Control" }, "space",  function () awful.spawn("rofi -show drun -show-icons -icon-theme Boston") end,
               {description = "open application launcher", group = "awesome"}),
     
     -- Rofi Window change
