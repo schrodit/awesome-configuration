bf60d62afa047fa09c76a6b80ad0dac8b7330c28
schrodit <mail@timschrodi.tech>
2021-12-16 15:49:59 +0100

    add background and brightness

--- install_de.sh
+++ install_de.sh
@@ -5,13 +5,12 @@ set -e
 set -o pipefail
 
 THEME_DIR="$(realpath $(dirname "$0"))"
-
 wd="$(pwd)"
 
 source "$THEME_DIR/helper.sh"
 
 echo "Install X Server"
-installWithPacman xorg-server xorg-xrandr xorg-xinput xbindkeys xsel
+installWithPacman xorg-server xorg-xrandr xorg-xinput xbindkeys xsel acpilight
 sudo cp "$THEME_DIR/xserver/00-keyboard.conf" /etc/X11/xorg.conf.d/00-keyboard.conf
 createSymlink "$THEME_DIR/xserver/.xbindkeysrc" "$HOME/.xbindkeysrc"
 
@@ -27,5 +26,7 @@ installWithPacman lightdm lightdm-webkit2-greeter lightdm-webkit-theme-litarvan
 
 sudo cp "$THEME_DIR/lightdm/lightdm.conf" /etc/lightdm/lightdm.conf
 sudo cp "$THEME_DIR/lightdm/lightdm-webkit2-greeter.conf" /etc/lightdm/lightdm-webkit2-greeter.conf
+
+sudo cp "$THEME_DIR/lightdm/sunset.jpg" /usr/share/backgrounds/sunset.jpg
 
 sudo systemctl enable lightdm
